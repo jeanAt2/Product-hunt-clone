89e5a4eb795bc0fd83c06b7b47e1a812e09b7c7f
Jean Carlos dos Santos <jean0king@gmail.com>
2020-04-03 11:22:01 -0300

    Last Commit backEnd

--- node-api/server.js
+++ node-api/server.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+
+//iniciando o App;
 const app = express();
+app.use(express.json());//Usar ojson para enviar dados para o DB
+app.use(cors());
 
-app.get('/',(request, response)=>{
-  response.send("Hello Rocketseat");
-});
+//iniciando o DB
+
+mongoose.connect('mongodb://192.168.99.100:27017/nodeapi', 
+{ useUnifiedTopology: true,
+  useNewUrlParser: true }
+);
+
+requireDir('./src/models/');
+
+//Rotas
+app.use('/api',require('./src/routes'));
 
 app.listen(3333);
